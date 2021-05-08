acba36ab18f9de8e74c1c8ff076ab9ee8582216a
Joshua Mirth <19397711+joshuamirth@users.noreply.github.com>
2021-05-08 15:24:58 -0400

    Display all T Scores.

--- app.R
+++ app.R
@@ -9,12 +9,16 @@
 
 library(shiny)
 
-# Data preprocessing ----
-# Non-reactive functions to set up the dataset.
+##############################################################################
+# Data pre-processing.
+##############################################################################
+# Import the data set from the database. (Currently just
+# a flat R data frame.)
 df <- readRDS("data/lu_data.Rda")
-# Compute ASEBA statistics for all athletes in dataset.
-# Three categories: Adaptive Functioning, Syndrome, and DSM-Oriented scale scores.
-# Build each as a separate dataframe.
+
+# Compute ASEBA statistics for all athletes in dataset. Three categories:
+# Adaptive Functioning, Syndrome, and DSM-Oriented scale scores. Build each as a
+# separate dataframe.
 
 # Adaptive Functioning Scale Scores
 afCats <- c("Friends", "Spouse/Partner", "Family", "Job", "Education",
@@ -23,8 +27,16 @@ afCols <- c("Friends_TScore", "Spouse_Partner_TScore", "Family_TScore",
            "Job_TScore", "Education_TScore", "Mean_Adaptive_TScore",
            "Personal_Strengths_TScore")
 afT_ScoreAverage <- colMeans(df[,afCols], na.rm = T)
-adaptiveFunctioning <- data.frame(afCats, afT_ScoreAverage)
-colnames(adaptiveFunctioning) <- c("Category", "All Athlete T Score")
+
+# Function to create table of individual, team, and all adaptive functioning values.
+buildAdaptiveFunctioning <- function(athlete, team){
+    afT_ScoreAthlete <- as.numeric(athlete[afCols])
+    afT_ScoreTeam <- colMeans(team[,afCols], na.rm=T)
+    adaptiveFunctioning <- data.frame(afCats, afT_ScoreAverage, afT_ScoreTeam, afT_ScoreAthlete)
+    colnames(adaptiveFunctioning) <- c("Category", "All Athletes T Score Average",
+                            "Teammates' T Score Average", "Individual T Score")
+    return(adaptiveFunctioning)
+}
 
 # Syndrome Scale Scores
 ssCats <- c("Anxious/Depressed", "Withdrawn", "Somatic Complaints",
@@ -35,9 +47,18 @@ ssCols <- c("Anxious__Depressed_TScore", "Withdrawn_TScore",
             "Attention_Problems_TScore", "Aggressive_Behavior_TScore",
             "Rule_Breaking_Behavior_TScore", "Intrusive_TScore")
 ssT_ScoreAverage <- colMeans(df[,ssCols], na.rm = T)
-syndrome <- data.frame(ssCats, ssT_ScoreAverage)
-colnames(syndrome) <- c("Category", "All Athlete T Score")
 
+# Function to create table of individual, team, and all syndrome values.
+buildSyndrome <- function(athlete, team){
+    ssT_ScoreAthlete <- as.numeric(athlete[ssCols])
+    ssT_ScoreTeam <- colMeans(team[,ssCols], na.rm=T)
+    syndrome <- data.frame(ssCats, ssT_ScoreAverage, ssT_ScoreTeam, ssT_ScoreAthlete)
+    colnames(syndrome) <- c("Category", "All Athletes T Score Average",
+                            "Teammates' T Score Average", "Individual T Score")
+    return(syndrome)
+}
+
+# DSM-Oriented Scale Scores
 dsmCats <- c("Depressive Problems", "Anxiety Problems", "Somatic Problems",
              "Avoidant Personality Problems", "AD/H Problems",
              "Antisocial Personality")
@@ -45,10 +66,20 @@ dsmCols <- c("Depressive_Problems_TScore", "Anxiety_Problems_TScore",
              "Somatic_Problems_TScore", "Avoidant_Personality_Problems_TScore",
              "AD_H_Problems_TScore", "Antisocial_Personality_TScore")
 dsmT_ScoreAverage <- colMeans(df[,dsmCols], na.rm = T)
-dsmOriented <- data.frame(dsmCats, dsmT_ScoreAverage)
-colnames(dsmOriented) <- c("Category", "All Athlete T Score")
 
-# Define UI for application that draws a histogram
+# Function to create table of individual, team, and all DSM Oriented values.
+buildDSMOriented <- function(athlete, team){
+    dsmT_ScoreAthlete <- as.numeric(athlete[dsmCols])
+    dsmT_ScoreTeam <- colMeans(team[,dsmCols], na.rm=T)
+    dsmOriented <- data.frame(dsmCats, dsmT_ScoreAverage, dsmT_ScoreTeam, dsmT_ScoreAthlete)
+    colnames(dsmOriented) <- c("Category", "All Athletes T Score Average",
+                            "Teammates' T Score Average", "Individual T Score")
+    return(dsmOriented)
+}
+
+##############################################################################
+# UI
+##############################################################################
 ui <- fluidPage(
 
     # Application title
@@ -61,52 +92,45 @@ ui <- fluidPage(
             selectInput("team", "Team:", df$TEAM),
             
             uiOutput("athleteSelection"),
-            
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+           
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot"),
            tableOutput("adaptiveFunctioningTable"),
            tableOutput("syndromeTable"),
            tableOutput("dsmOrientedTable"),
-           tableOutput("teamTable")
         )
+        
     )
 )
 
-# Define server logic required to draw a histogram
+##############################################################################
+# Server
+##############################################################################
 server <- function(input, output) {
 
     # Rows for the team chosen
     selectedTeamIdx <- reactive(which(df$TEAM == input$team))
+    selectedAthleteIdx <- reactive(which.min(df$NAME == input$athlete))
     selectedTeam <- reactive(df[selectedTeamIdx(),])
+    selectedAthlete <- reactive(df[selectedAthleteIdx(),])
     
     output$athleteSelection <- renderUI({
         selectInput("athlete", "Athlete:",
                     choices = unique(df$NAME[selectedTeamIdx()]))
     })
     
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- na.omit(df$Total_Problems_TScore)
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-    
     # Tables of T-scores for all athletes.
-    output$adaptiveFunctioningTable <- renderTable(adaptiveFunctioning)
-    output$dsmOrientedTable <- renderTable(dsmOriented)
-    output$syndromeTable <- renderTable(syndrome)
-    
-    output$teamTable <- renderTable(head(selectedTeam()))
+    output$adaptiveFunctioningTable <- renderTable(
+        buildAdaptiveFunctioning(selectedAthlete(), selectedTeam())
+    )
+    output$dsmOrientedTable <- renderTable(
+        buildDSMOriented(selectedAthlete(), selectedTeam())
+    )
+    output$syndromeTable <- renderTable(
+        buildSyndrome(selectedAthlete(), selectedTeam())
+    )
     
 }
 
