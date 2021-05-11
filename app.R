2290fe0024f2c772611ec08bc838c8e9db39f6ff
Joshua Mirth <19397711+joshuamirth@users.noreply.github.com>
2021-05-10 20:11:06 -0400

    Radio button for stat type.

--- app.R
+++ app.R
@@ -20,7 +20,7 @@ df <- readRDS("data/lu_data.Rda")
 # Adaptive Functioning, Syndrome, and DSM-Oriented scale scores. Build each as a
 # separate dataframe.
 
-# Adaptive Functioning Scale Scores
+# Adaptive Functioning
 afCats <- c("Friends", "Spouse/Partner", "Family", "Job", "Education",
                 "Mean Adaptive", "Personal Strengths")
 afCols <- c("Friends_TScore", "Spouse_Partner_TScore", "Family_TScore",
@@ -28,17 +28,7 @@ afCols <- c("Friends_TScore", "Spouse_Partner_TScore", "Family_TScore",
            "Personal_Strengths_TScore")
 afT_ScoreAverage <- colMeans(df[,afCols], na.rm = T)
 
-# Function to create table of individual, team, and all adaptive functioning values.
-buildAdaptiveFunctioning <- function(athlete, team){
-    afT_ScoreAthlete <- as.numeric(athlete[afCols])
-    afT_ScoreTeam <- colMeans(team[,afCols], na.rm=T)
-    adaptiveFunctioning <- data.frame(afCats, afT_ScoreAverage, afT_ScoreTeam, afT_ScoreAthlete)
-    colnames(adaptiveFunctioning) <- c("Category", "All Athletes T Score Average",
-                            "Teammates' T Score Average", "Individual T Score")
-    return(adaptiveFunctioning)
-}
-
-# Syndrome Scale Scores
+# Syndromes
 ssCats <- c("Anxious/Depressed", "Withdrawn", "Somatic Complaints",
             "Thought Problems", "Attention Problems", "Aggressive Behavior",
             "Rule-Breaking Behavior", "Intrusive")
@@ -48,17 +38,7 @@ ssCols <- c("Anxious__Depressed_TScore", "Withdrawn_TScore",
             "Rule_Breaking_Behavior_TScore", "Intrusive_TScore")
 ssT_ScoreAverage <- colMeans(df[,ssCols], na.rm = T)
 
-# Function to create table of individual, team, and all syndrome values.
-buildSyndrome <- function(athlete, team){
-    ssT_ScoreAthlete <- as.numeric(athlete[ssCols])
-    ssT_ScoreTeam <- colMeans(team[,ssCols], na.rm=T)
-    syndrome <- data.frame(ssCats, ssT_ScoreAverage, ssT_ScoreTeam, ssT_ScoreAthlete)
-    colnames(syndrome) <- c("Category", "All Athletes T Score Average",
-                            "Teammates' T Score Average", "Individual T Score")
-    return(syndrome)
-}
-
-# DSM-Oriented Scale Scores
+# DSM-Oriented
 dsmCats <- c("Depressive Problems", "Anxiety Problems", "Somatic Problems",
              "Avoidant Personality Problems", "AD/H Problems",
              "Antisocial Personality")
@@ -67,14 +47,21 @@ dsmCols <- c("Depressive_Problems_TScore", "Anxiety_Problems_TScore",
              "AD_H_Problems_TScore", "Antisocial_Personality_TScore")
 dsmT_ScoreAverage <- colMeans(df[,dsmCols], na.rm = T)
 
-# Function to create table of individual, team, and all DSM Oriented values.
-buildDSMOriented <- function(athlete, team){
-    dsmT_ScoreAthlete <- as.numeric(athlete[dsmCols])
-    dsmT_ScoreTeam <- colMeans(team[,dsmCols], na.rm=T)
-    dsmOriented <- data.frame(dsmCats, dsmT_ScoreAverage, dsmT_ScoreTeam, dsmT_ScoreAthlete)
-    colnames(dsmOriented) <- c("Category", "All Athletes T Score Average",
-                            "Teammates' T Score Average", "Individual T Score")
-    return(dsmOriented)
+# Function to create table of individual, team, and all test score values.
+buildStatsTable <- function(athlete, team, cols, cats, tScoreAverage, statType="T_Score"){
+    tScoreAthlete <- as.numeric(athlete[cols])
+    tScoreTeam <- colMeans(team[,cols], na.rm=T)
+    if(statType == "T_Score"){
+        tableFrame <- data.frame(cats, tScoreAverage, tScoreTeam, tScoreAthlete)
+        colnames(tableFrame) <- c("Category", "All Athletes T Score Average",
+                            "Teammates' T Score Average", "Individual T Score")    
+    } else {
+        tableFrame <- data.frame(cats, 100*pnorm(tScoreAverage, 50, 10),
+            100*pnorm(tScoreTeam, 50, 10), 100*pnorm(tScoreAthlete, 50, 10))
+            colnames(tableFrame) <- c("Category", "All Athletes Percentile Average",
+                            "Teammates' Percentile Average", "Individual Percentile")    
+    }
+    return(tableFrame)
 }
 
 ##############################################################################
@@ -92,6 +79,8 @@ ui <- fluidPage(
             selectInput("team", "Team:", df$TEAM),
             
             uiOutput("athleteSelection"),
+            
+            radioButtons("statType", "Display Style:", c("T Score" = "T_Score", "Percentile" = "Percentile"))
            
         ),
 
@@ -112,24 +101,32 @@ server <- function(input, output) {
 
     # Rows for the team chosen
     selectedTeamIdx <- reactive(which(df$TEAM == input$team))
-    selectedAthleteIdx <- reactive(which.min(df$NAME == input$athlete))
+    # TODO: currently selecting last index at which an athlete occurs. There are
+    # athletes with multiple occurrences. Decide how to handle that. The best
+    # option might be a third drop-down menu to select date of assessment to
+    # view. Another option might be to always show most recent (which is
+    # probably, but not necessarily, the last index as it is currently.)
+    selectedAthleteIdx <- reactive(which.max(df$NAME == input$athlete))
     selectedTeam <- reactive(df[selectedTeamIdx(),])
     selectedAthlete <- reactive(df[selectedAthleteIdx(),])
-    
+   
     output$athleteSelection <- renderUI({
         selectInput("athlete", "Athlete:",
                     choices = unique(df$NAME[selectedTeamIdx()]))
     })
     
-    # Tables of T-scores for all athletes.
+    # Tables of T-scores/Percentiles compared with all athletes and teammates.
     output$adaptiveFunctioningTable <- renderTable(
-        buildAdaptiveFunctioning(selectedAthlete(), selectedTeam())
+        buildStatsTable(selectedAthlete(), selectedTeam(), afCols, afCats,
+                        afT_ScoreAverage, statType = input$statType)
     )
     output$dsmOrientedTable <- renderTable(
-        buildDSMOriented(selectedAthlete(), selectedTeam())
+        buildStatsTable(selectedAthlete(), selectedTeam(), dsmCols, dsmCats,
+                        dsmT_ScoreAverage, statType = input$statType)
     )
     output$syndromeTable <- renderTable(
-        buildSyndrome(selectedAthlete(), selectedTeam())
+        buildStatsTable(selectedAthlete(), selectedTeam(), ssCols, ssCats,
+                        ssT_ScoreAverage, statType = input$statType)
     )
     
 }
